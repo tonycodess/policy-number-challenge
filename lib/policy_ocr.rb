feb8b50af493530f32754401f3cf8dddac6cd820
tonycodes <tonyperkinscon@gmail.com>
2023-07-17 22:08:43 +0500

    basic parsing of text added checksum added writing findings in file added test cases added of first 3 stories

--- lib/policy_ocr.rb
+++ lib/policy_ocr.rb
@@ -1,2 +1,88 @@
+# frozen_string_literal: true
+
 module PolicyOcr
+  class ScanOcr
+    def extract_number(file_path)
+      extracted_number = parse_text(file_path)
+      write_parsed_numbers(extracted_number)
+      extracted_number
+    end
+
+    def parse_text(file_path)
+      digit_mapping = {
+        ' _ | ||_|' => '0',
+        '     |  |' => '1',
+        ' _  _||_ ' => '2',
+        ' _  _| _|' => '3',
+        '   |_|  |' => '4',
+        ' _ |_  _|' => '5',
+        ' _ |_ |_|' => '6',
+        ' _   |  |' => '7',
+        ' _ |_||_|' => '8',
+        ' _ |_| _|' => '9'
+      }
+
+      digit_size = 3
+      digits = []
+      extracted_number = []
+
+      File.open(file_path, 'r') do |file|
+        while !file.eof?
+          lines = file.take(4)
+          lines[0].each_char.with_index do |_, obj|
+            next if obj % 3 != 0
+
+            digit = lines[0][obj, digit_size] + lines[1][obj, digit_size] + lines[2][obj, digit_size]
+            digits << (digit_mapping[digit].nil? ? '?' : digit_mapping[digit])
+          end
+          number = digits.join('').chop
+          digits.clear
+          extracted_number << number
+        end
+      end
+      extracted_number
+    end
+
+    def write_parsed_numbers(extracted_number)
+      File.open('./spec/fixtures/parsed_numbers.txt', 'w') do |file|
+        extracted_number.each do |number|
+          file.write("#{number} \n")
+        end
+      end
+    end
+
+    def write_findings(file_path)
+      extracted_number = extract_number(file_path)
+
+      File.open('./spec/fixtures/findings.txt', 'w') do |file|
+        extracted_number.each do |number|
+          file.write("#{number} #{evaluate_number_tag(number)} \n")
+        end
+      end
+    end
+
+    def evaluate_number_tag(number)
+      if !legal_number?(number)
+        'ILL'
+      elsif !valid_checksum?(number)
+        'ERR'
+      else
+        ''
+      end
+    end
+
+    def legal_number?(number)
+      number.to_s.chars.all? { |digit| digit.to_i.to_s == digit }
+    end
+
+    def valid_checksum?(policy_number)
+      weights = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+      digits = policy_number.to_s.chars.map(&:to_i)
+
+      checksum = digits.each_with_index.sum { |digit, index| digit * weights[index] }
+      (checksum % 11).zero?
+    end
+  end
+  # ScanOcr.new.extract_number('./spec/fixtures/sample.txt')
 end
+  
--- spec/policy_ocr_spec.rb
+++ spec/policy_ocr_spec.rb
@@ -1,11 +1,50 @@
+# frozen_string_literal: true
+
 require_relative '../lib/policy_ocr'
 
-describe PolicyOcr do
-  it "loads" do
-    expect(PolicyOcr).to be_a Module
+RSpec.describe PolicyOcr::ScanOcr do
+  let(:scan) { PolicyOcr::ScanOcr.new }
+
+  it "loads Policy Module" do
+    expect(PolicyOcr).to be_a(Module)
+  end
+
+  it "loads Scan Class" do
+    expect(described_class).to be_a(Class)
   end
 
   it 'loads the sample.txt' do
     expect(fixture('sample').lines.count).to eq(44)
+  end
+
+  it 'checks the single number' do
+    extracted_number = scan.extract_number('./spec/fixtures/single_number.txt').first.to_i
+    expect(extracted_number).to eq(123456789)
+  end
+
+  it 'checks multiple numbers' do
+    extracted_number = scan.extract_number('./spec/fixtures/multiple_numbers.txt')
+    valid_numbers = extracted_number.all? { |number| number.chars.map(&:to_i).join('').eql?(number) }
+    expect(valid_numbers).to eq(true)
+  end
+
+  it 'checks the number is legal' do
+    expect(scan.legal_number?("987654321")).to eq(true)
+  end
+
+  it 'checks the number is illegal' do
+    expect(scan.legal_number?("9876543?1")).to eq(false)
+  end
+
+  it 'checks the number with valid checksum' do
+    expect(scan.valid_checksum?("345882865")).to eq(true)
+  end
+
+  it 'checks the number with invalid checksum' do
+    expect(scan.valid_checksum?("245882860")).to eq(false)
+  end
+
+  it 'write findings' do
+    scan.write_findings('./spec/fixtures/invalid_numbers.txt')
   end
 end
